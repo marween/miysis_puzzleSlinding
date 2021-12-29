4eb21fcdeccc6250da4884c00b3b0a4d9e28287f
Magali <annemagali.saintgeorges@gmail.com>
2021-12-29 17:14:08 +0100

    Updates

---
+++ precache-manifest.92b378177b2d6a5449e8d14977d7c496.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e3470ff59ec7f6e7fd710f508788b4",
+    "url": "/miysis_puzzleSlinding/index.html"
+  },
+  {
+    "revision": "add2b9c286ce04a4c0db",
+    "url": "/miysis_puzzleSlinding/static/css/2.f7be851e.chunk.css"
+  },
+  {
+    "revision": "1b8e76e04a521992d588",
+    "url": "/miysis_puzzleSlinding/static/css/main.e0eba3c4.chunk.css"
+  },
+  {
+    "revision": "add2b9c286ce04a4c0db",
+    "url": "/miysis_puzzleSlinding/static/js/2.a55fb585.chunk.js"
+  },
+  {
+    "revision": "1b8e76e04a521992d588",
+    "url": "/miysis_puzzleSlinding/static/js/main.d95ad16d.chunk.js"
+  },
+  {
+    "revision": "aaed800328b86c79fc3f",
+    "url": "/miysis_puzzleSlinding/static/js/runtime-main.46a46d48.js"
+  },
+  {
+    "revision": "40486cf58cf2773eb6a90e0cbb01b20a",
+    "url": "/miysis_puzzleSlinding/static/media/Puzzle.40486cf5.jpg"
+  },
+  {
+    "revision": "5d4a3c7acd04643e7db5fcfedaf10073",
+    "url": "/miysis_puzzleSlinding/static/media/pexels-photo-2017481.5d4a3c7a.jpg"
+  },
+  {
+    "revision": "317381e1eaf15d644777cc79f700596d",
+    "url": "/miysis_puzzleSlinding/static/media/pexels-photo-2582937.317381e1.jpg"
+  },
+  {
+    "revision": "1c8a479417e0c064f77a83a187e744cd",
+    "url": "/miysis_puzzleSlinding/static/media/pexels-photo-267596.1c8a4794.jpg"
+  },
+  {
+    "revision": "c9658d6903e61ef6ba36afabf06f2423",
+    "url": "/miysis_puzzleSlinding/static/media/pexels-photo-724994.c9658d69.jpg"
+  },
+  {
+    "revision": "a106cba52722c2f43a80d98cd7741722",
+    "url": "/miysis_puzzleSlinding/static/media/pexels-photo-926987.a106cba5.jpg"
+  },
+  {
+    "revision": "b70f36af9351056e0056f3565dc07750",
+    "url": "/miysis_puzzleSlinding/static/media/photo-1555616635-640b71bdb185.b70f36af.jpg"
+  },
+  {
+    "revision": "0dca93b3359067584c7ccccf0fe69f5a",
+    "url": "/miysis_puzzleSlinding/static/media/photo-1559757148-5c350d0d3c56.0dca93b3.jpg"
+  }
+]);
